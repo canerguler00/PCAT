24779e5d390216f5d7512a10f91a79109789466a
caner guler <c.guler58@gmail.com>
2022-07-25 20:44:57 +0700

    Static files upload

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const fn = function (){
-    console.log("funcotion is working")
-};
+const express = require("express");
+const path = require("path")
+
+const app = express();
+
+
+//MIDDLEWARES
+app.use(express.static('public'))
+
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+
+    
+})
+
+const port =3000;
+app.listen(port, ()=>{
+    console.log(`Sunucu ${port} unda çalışmaya başladı...`);
+})
